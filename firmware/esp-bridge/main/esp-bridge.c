ff6c3ca1a1efde95de95c4cb18f4a1f5e16bf2f3
andreasbachmann <github.prolonged@turinturambar.com>
2026-04-12 17:57:43 +0200

    add bare bones wifi initialization to esp-bridge

--- firmware/esp-bridge/main/esp-bridge.c
+++ firmware/esp-bridge/main/esp-bridge.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+
+static const char *TAG = "ESPBRIDGE";
+
+static void initialize_wifi()
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_LOGI(TAG, "default event loop created");
+}
 
 void app_main(void)
 {
-
+    initialize_wifi();
 }
