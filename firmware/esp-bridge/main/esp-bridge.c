d19edcbd6f9f395b9dd7061da6647fbc587ca79b
andreasbachmann <github.prolonged@turinturambar.com>
2026-04-12 19:15:09 +0200

    add wifi event handler

--- firmware/esp-bridge/main/esp-bridge.c
+++ firmware/esp-bridge/main/esp-bridge.c
@@ -5,14 +5,80 @@
 #include "esp_event.h"
 #include "esp_wifi.h"
 
+#define WIFI_CONNECTED_BIT  BIT0
+#define WIFI_FAIL_BIT       BIT1
+
+// GLOBALS
 static const char *TAG = "ESPBRIDGE";
+
+// WIFI
+static EventGroupHandle_t       wifi_event_group;
+static int retries =            0;
+static const int MAX_RETRIES =  5;
+
+
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        ESP_LOGI(TAG, "trying to connect to wifi");
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (retries < MAX_RETRIES) {
+            ESP_LOGW(TAG, "trying to reconnect to wifi (retry %d)", (retries + 1));
+            esp_wifi_connect();
+            retries++;
+        } else {
+            xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
+            ESP_LOGE(TAG, "ran out of retries after %d connection attempts", MAX_RETRIES);
+        }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got IP:" IPSTR, IP2STR(&event->ip_info.ip));
+        retries = 0;
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
 
 static void initialize_wifi()
 {
+    wifi_event_group = xEventGroupCreate();
+
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_LOGI(TAG, "default event loop created");
+    esp_netif_create_default_wifi_sta();
+    
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_LOGI(TAG, "wifi initialized");
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_got_ip)); 
+    
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid =     CONFIG_WIFI_SSID,
+            .password = CONFIG_WIFI_PASSWORD
+        }
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "wifi started, waiting for connection");
 }
 
 void app_main(void)
